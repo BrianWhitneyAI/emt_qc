3b05d9fb512bde7e11845205b01125cc27cc9e14
aditnath <an.adityanath@gmail.com>
2022-02-17 15:15:54 -0800

    Modified pipeline_tools, fn that finds argo files is now more specific and has error messages built in

--- emt_qc/emt_block_duration/pipeline_tools.py
+++ emt_qc/emt_block_duration/pipeline_tools.py
@@ -1,16 +1,27 @@
-
 import os 
+from pathlib import Path
 
 def get_QC_daily_path(
-    refrence_directory: os.path, # use path to Argo_QC_daily
-    system: str,
-    search_criteria: list
-)-> os.path:
-    for (dirpath, dirnames, _) in os.walk(f'{refrence_directory}/{system}'):
-        for foldername in dirnames:
-            folder_metadata = foldername.split("_")
-            if all(item in folder_metadata for item in search_criteria) is True:
-                return f'{dirpath}/{foldername}/{foldername}.czi'
-    raise Exception("No file found with given critria: " + search_criteria)
-
-
+    reference_directory: str, # use path to Argo_QC_daily
+    system: str, # Options are ZSD0, ZSD1, ZSD2, ZSD3, 3i0, 3i1
+    objective: int,
+    date: int
+)-> Path:
+    opt_cont_files = []
+    for opt_dir in os.listdir(f'{reference_directory}/{system}'):
+        folder_metadata = opt_dir.split("_")
+        folder_metadata = [x.upper() for x in folder_metadata]
+        if all(x in folder_metadata for x in [system, f'{objective}X', str(date)]) is True:
+            opt_conts = [f for f in os.listdir(f'{reference_directory}/{system}/{opt_dir}') if f.endswith('.czi')]
+            for opt_cont in opt_conts:
+                opt_cont_files.append(Path(f'{reference_directory}/{system}/{opt_dir}/{opt_cont}'))
+    
+    if len(opt_cont_files) == 1:
+        return Path(opt_cont_files[0])
+    elif len(opt_cont_files) == 0:
+        raise Exception(f"No files found with system: {system}, objective: {objective}, date: {date}")
+    else:
+        print(f"Multiple files found with system: {system}, objective: {objective}, date: {date}. Printing all paths, and outputting the first found")
+        for i in opt_cont_files:
+            print(i)
+        return Path(opt_cont_files[0])
